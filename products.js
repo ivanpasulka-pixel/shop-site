0c370bd11700d0f49d3addf65a907df4197b603b
Ivan <ivanpasulka@gmail.com>
2026-07-01 13:12:21 +0300

    update

--- products.js
+++ products.js
@@ -2,8 +2,8 @@ const PRODUCTS = [
   {
     id: "hmp-suit",
     name: "Спортивный костюм HMP",
-    price: 1299,
-    oldPrice: 1899,
+    price: 999,
+    oldPrice: null,
     badge: "NEW",
     images: ["images/hmp-suit-1.jpg", "images/hmp-suit-2.jpg"],
     category: "sets",
@@ -14,8 +14,8 @@ const PRODUCTS = [
   {
     id: "cortiz-pants",
     name: "Спортивные штаны Cortiz",
-    price: 749,
-    oldPrice: 899,
+    price: 999,
+    oldPrice: null,
     badge: null,
     images: ["images/cortiz-pants-1.jpg"],
     category: "pants",
@@ -26,10 +26,14 @@ const PRODUCTS = [
   {
     id: "guerillaz-longsleeve",
     name: "Лонгслив Guerillaz",
-    price: 599,
+    price: 999,
     oldPrice: null,
     badge: "DROP",
-    images: ["images/guerillaz-longsleeve-1.jpg", "images/guerillaz-longsleeve-2.jpg", "images/guerillaz-longsleeve-3.jpg"],
+    images: [
+      "images/guerillaz-longsleeve-1.jpg",
+      "images/guerillaz-longsleeve-2.jpg",
+      "images/guerillaz-longsleeve-3.jpg"
+    ],
     category: "tops",
     sizes: ["S", "M", "L", "XL"],
     inStock: true,
@@ -38,12 +42,11 @@ const PRODUCTS = [
   {
     id: "guess-bag",
     name: "Сумка через плечо GUESS",
-    price: 1099,
-    oldPrice: 1399,
-    badge: "SALE",
+    price: 999,
+    oldPrice: null,
+    badge: null,
     images: ["images/guess-bag-1.jpg", "images/guess-bag-2.jpg"],
     category: "bags",
-    sizes: ["One Size"],
     inStock: true,
     description: "Компактная сумка через плечо для повседневных выходов."
   }
